96244c3fffa0cfe47cde788b0cb4804a504d0439
JuanFuent.es <input@juanfuent.es>
2025-08-30 23:19:03 -0600

    Actualicé método para crear tarea, y vista para listar tareas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,22 +10,23 @@ def nueva_tarea():
     if request.method == "POST":
         nombre = request.form["title"]
         # return redirect("/tareas")
-        return f"La tarea nueva es: {nombre}"
+        tareas = request.cookies.get("tareas", "")
+        # Ej: Cookie:tareas = "Barrer,Limpiar,Estudiar"
+        lista = tareas.split(",") if tareas else []
+        lista.append(nombre)
+        # Ej: Cookie:tareas = ["Barrer","Limpiar","Estudiar","Hacer ejercicio"]
+        response = make_response(redirect("/tareas"))
+        # Ej: Cookie:tareas = "Barrer,Limpiar,Estudiar,Hacer ejercicio"
+        response.set_cookie("tareas", ",".join(lista))
+        return response
     else:
         return render_template("formulario.html")
 
 @app.route("/tareas")
 def mostrar_tareas():
-    tareas = [
-        {
-            "nombre": "Hacer ejercicio",
-            "estado": "pendiente",
-            "prioridad": "alta",
-            "fecha": "2025-08-24",
-            "completada": False
-        },
-    ]
-    return render_template("tareas.html", tareas=tareas)
+    tareas = request.cookies.get("tareas", "")
+    lista_tareas = tareas.split(",") if tareas else []
+    return render_template("tareas.html", tareas=lista_tareas)
 
 @app.route('/acerca-de')
 def acerca_de():
