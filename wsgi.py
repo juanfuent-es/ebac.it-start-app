a396b8c4efab2ce486747d93be7beed7a901806a
JuanFuent.es <input@juanfuent.es>
2025-09-19 00:52:27 -0600

    Update wsgi

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,3 @@ from app import app
 
 # Render necesita que la variable se llame 'application'
 application = app
-
-# Si se ejecuta directamente, iniciar el servidor de desarrollo
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
