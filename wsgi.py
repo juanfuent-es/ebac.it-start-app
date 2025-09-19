b156f4b83495e0f9ffd6492494cbabaaabe5f6a5
JuanFuent.es <input@juanfuent.es>
2025-09-19 14:52:41 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
-# 
+#!/usr/bin/env python3
+"""
+Archivo WSGI para deploy en Render.com
+Este archivo permite que Render ejecute la aplicación Flask
+"""
+
+# Importar la aplicación Flask desde app.py
 from app import app
 
+# Render necesita que la variable se llame 'application'
 application = app
